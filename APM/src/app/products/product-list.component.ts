00459f82b84c34c82f27571880761f6375536f79
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-06 16:56:43 +0400

    Added images and styles

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] = [
         {
             'productId': 1,
@@ -16,7 +18,7 @@ export class ProductListComponent {
             'description': 'Leaf rake with 48-inch wooden handle.',
             'price': 19.95,
             'starRating': 3.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+            'imageUrl': 'https://www.beatsons.co.uk/images/faithfull-leaf-rake-fibreglass-shaft-p7564-30639_medium.jpg'
         },
         {
             'productId': 2,
@@ -26,7 +28,8 @@ export class ProductListComponent {
             'description': '15 gallon capacity rolling garden cart',
             'price': 32.99,
             'starRating': 4.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+            // tslint:disable-next-line:max-line-length
+            'imageUrl': 'https://2ecffd01e1ab3e9383f0-07db7b9624bbdf022e3b5395236d5cf8.ssl.cf4.rackcdn.com/Product-800x800/4398f5f4-08a3-4025-8241-c6289a9cd9c1.jpg'
         }
     ];
 }
