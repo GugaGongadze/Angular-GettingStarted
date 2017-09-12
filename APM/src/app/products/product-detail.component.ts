6464bf49ce5c61cf59ceb26222aee6f8512d5755
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-12 16:44:47 +0400

    Added routing to product details

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -10,9 +11,21 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': 1,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'https://www.beatsons.co.uk/images/faithfull-leaf-rake-fibreglass-shaft-p7564-30639_medium.jpg'
+    }
   }
 
 }
