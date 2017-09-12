14cf8b0e1baa23805c3df945933cbcf2f65d216b
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-12 15:54:06 +0400

    Added navigation to home page

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -5,8 +5,15 @@ import { ProductService } from './products/product.service';
   selector: 'pm-root',
   template: `
     <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
+      <nav class='navbar navbar-default'>
+        <div class='container-fluid'>
+          <a class='navbar-brand'>{{ pageTitle }}</a>
+          <ul class='nav navbar-nav'>
+            <li><a [routerLink]="['/welcome']">Home</a></li>
+            <li><a [routerLink]="['/products']">Product List</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
   `,
   providers: [ ProductService ]
