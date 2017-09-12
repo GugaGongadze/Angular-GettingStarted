9926e2347b5f99ef7b0c5a2224ba972656a5497e
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-12 16:52:39 +0400

    Added back button and routing with code

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -11,9 +11,11 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
+    // tslint:disable-next-line:prefer-const
     let id = +this._route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
     this.product = {
@@ -25,7 +27,11 @@ export class ProductDetailComponent implements OnInit {
       'price': 19.95,
       'starRating': 3.2,
       'imageUrl': 'https://www.beatsons.co.uk/images/faithfull-leaf-rake-fibreglass-shaft-p7564-30639_medium.jpg'
-    }
+    };
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
