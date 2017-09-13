90635f7feb9b29e51bfeed47322e1c8ab668ea85
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-13 16:00:49 +0400

    Added shared module and further decomposed the code

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,31 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StarComponent } from '../shared/star.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { ProductGuardService } from './product-guard.service';
 import { ProductService } from './product.service';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
         canActivate: [ProductGuardService],
         component: ProductDetailComponent
       },
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ],
   providers: [
     ProductService,
