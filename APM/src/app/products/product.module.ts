6398cc2e2d4b25805529513843fae0e71e31ffbf
EUROPE-BET\ggongadze <ggongadze@europe-bet.com>
2017-09-13 15:51:42 +0400

    Added new module and redistributed elements

---
+++ APM/src/app/products/product.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarComponent } from '../shared/star.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductListComponent } from './product-list.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProductGuardService } from './product-guard.service';
+import { ProductService } from './product.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',
+        canActivate: [ProductGuardService],
+        component: ProductDetailComponent
+      },
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  providers: [
+    ProductService,
+    ProductGuardService
+  ]
+})
+export class ProductModule { }
